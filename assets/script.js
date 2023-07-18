3547c1aae7b86a32d24a07a07bad9b56de281157
L.J Solis <l.jsolis@L.lan>
2023-07-18 11:25:27 -0500

    Another script js file appeared in my folder. Updated the correct js file with comments.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
 // Wraps all the code that interacts with the DOM in a jQuery call.
 $(function () {
 
-
+// Created variables that will be used to display the date, time block, and when someone clicks the save button.
   var dateEl = $("#currentDay")
   var timeBlock = $(".time-block")
   var saveBtn = $(".saveBtn")
 
+// Displays how the date will be displayed on the webpage in the header. Example July 18 2023. 
   dateEl.text(dayjs().format("MMMM D YYYY"))
   
+// Changes depending on the current time and will display each time block either in the past, present, or future with a different color.  
   timeBlock.each(function () {
     var currentTime = dayjs().hour()
     var timeBlockId = $(this).attr("id").slice(5)
@@ -19,7 +21,8 @@ $(function () {
       $(this).children(".description").attr("class", "col-8 col-md-10 description present")
     }  
   })
- 
+
+// The save button function will save what the user enters in the textarea to the localStorage. The button must be clicked in order for the description to be saved.
   saveBtn.on("click", function (event) {
     event.preventDefault()
     var key = $(this).parent().attr("id")
@@ -28,6 +31,7 @@ $(function () {
     localStorage.setItem(key, JSON.stringify(value))
   })
   
+// The for loop will retrieve what the user has entered into the textarea from localStorage for each time block.
   for (var i = 9; i <= 17; i++) {
     $(`#hour-${i} textarea`).val(JSON.parse(localStorage.getItem(`hour-${i}`)))
   }
